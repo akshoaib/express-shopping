9811b823519b25380316c381651dd5ced00e01c6
akshoaib <88835537+akshoaib@users.noreply.github.com>
2025-03-16 16:43:05 +0500

    allowed origin

--- index.ts
+++ index.ts
@@ -14,14 +14,16 @@ const app: Application = express();
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://your-live-frontend.com",
-];
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
