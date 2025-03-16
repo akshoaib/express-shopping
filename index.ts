23b77c82a6a199aba393033806a9d42eb54f7674
akshoaib <88835537+akshoaib@users.noreply.github.com>
2025-03-16 16:25:12 +0500

    allowed origin

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
