6af3a1b30fad72ebc8a3ff6aeaf4aad4f6990def
shoaib <shoaibakhtar@aussourceenterprises.com>
2025-04-08 09:59:24 +0500

    user controller refactored

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -6,18 +6,15 @@ import { createSecretToken } from "../utils";
 
 const userSignup = async (req: Request, res: Response): Promise<Response> => {
   const { firstName, lastName, email, password, role } = req.body;
-  console.log(req.body);
+
   const userExists: IUser | null = await User.findOne({ email });
-  console.log({ userExists });
 
   if (userExists) {
     return res.json({ message: "user already exists" });
   }
-  console.log({ password });
 
   const saltRounds = 12;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
-  console.log({ hashedPassword });
 
   const user = (await User.create({
     firstName,
@@ -44,23 +41,19 @@ const userSignup = async (req: Request, res: Response): Promise<Response> => {
 const login = async (req: Request, res: Response): Promise<Response> => {
   const { email, password } = req.body;
   const user: IUser | null = await User.findOne({ email });
-  console.log("ffound::", user);
 
   if (!user) {
     return res.status(400).json({ message: "user not found" });
   }
-  console.log({ password });
 
   const isMatch: boolean = await bcrypt.compare(password, user.password);
-
-  console.log({ isMatch });
 
   if (!isMatch) {
     return res.status(400).json({ message: "invalid email or password" });
   }
 
   const token = createSecretToken({ _id: user._id, role: user.role });
-  console.log({ user });
+
   return res.status(201).json({
     message: "User signed in successfully",
     success: true,
