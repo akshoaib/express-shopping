2c18077b1b8b7169edefbf1581599269eaa5e82d
akshoaib <akhtarshoaib317@gmail.com>
2026-02-10 12:25:23 +0500

    comment

--- routes/support.routes.ts
+++ routes/support.routes.ts
@@ -2,6 +2,6 @@ import express from "express";
 // import { chatWithBot } from "../controllers/chatbot.controller";
 
 const router = express.Router();
-
+//
 // router.post("/chatbot", chatWithBot);
 export default router;
